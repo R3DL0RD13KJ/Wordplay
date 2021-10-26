0edb3f736112b6874a03d935ca98710026a68a1e
Kenneth Johnson <kd123johnson@gmail.com>
2021-10-26 10:34:33 -0600

    MVP

---
+++ Wordplay/OmegaDelta.swift
@@ -0,0 +1,29 @@
+//
+//  OmegaDelta.swift
+//  Wordplay
+//
+//  Created by Kenneth Johnson on 10/26/21.
+//
+
+import UIKit
+
+class OmegaDelta: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Wordplay/ViewController.swift
+++ Wordplay/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var myUncle: UILabel!
+    
+    @IBOutlet weak var whatsLife: UILabel!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    
+    @IBAction func completeSentence(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "Alert!", message: "Finish the sentence you Wanker!", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(ok)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 
 }
 
